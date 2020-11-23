72089a6747f1a53c08e1a5ca9ccf128f10c592cf
cowdingus <zaki.nadhif45@gmail.com>
2020-11-23 20:34:03 +0700

    Added LineGraph

--- include/LineGraph.hpp
+++ include/LineGraph.hpp
@@ -3,6 +3,7 @@
 #include "CartesianGraph.hpp"
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/PrimitiveType.hpp>
 
 class LineGraph : public CartesianGraph
 {
@@ -17,14 +18,21 @@ public:
 	void removePoint(std::size_t index);
 	void clearPoints();
 
+	void setLineThickness(float thickness);
+	float getLineThickness() const;
+
+	void setLineColor(sf::Color color);
+	sf::Color getLineColor() const;
+
 private:
+	sf::Color lineColor = sf::Color::White;
+	float lineThickness = 1.f;
+
 	std::vector<sf::Vector2f> points;
 
-	sf::VertexArray mesh = sf::VertexArray(sf::Lines);
+	sf::VertexArray mesh = sf::VertexArray(sf::TriangleStrip);
 
-	bool needUpdate = true;
-
-	void createLine(const sf::Vector2f& from, const sf::Vector2f& to);
+	std::array<sf::Vector2f, 2> createLineSegment(const sf::Vector2f& point);	
 
 	void render(sf::RenderTexture& texture) const override;
 	void updateContent() override;
--- src/LineGraph.cpp
+++ src/LineGraph.cpp
@@ -21,7 +21,7 @@ sf::Vector2f LineGraph::getPoint(std::size_t index) const
 
 void LineGraph::addPoint(const sf::Vector2f &point)
 {
-	points.push_back(point);
+	points.emplace_back(point);
 	needUpdate = true;
 }
 
@@ -37,20 +37,64 @@ void LineGraph::clearPoints()
 	needUpdate = true;
 }
 
-void LineGraph::createLine(const sf::Vector2f& from, const sf::Vector2f& to)
+void LineGraph::setLineThickness(float thickness)
 {
-	
+	lineThickness = thickness;
+	needUpdate = true;
+}
+
+float LineGraph::getLineThickness() const
+{
+	return lineThickness;
+}
+
+void LineGraph::setLineColor(sf::Color color)
+{
+	lineColor = color;
+	for (std::size_t i = 0; i < mesh.getVertexCount(); ++i)
+	{
+		mesh[i].color = lineColor;
+	}
+}
+
+sf::Color LineGraph::getLineColor() const
+{
+	return lineColor;
+}
+
+std::array<sf::Vector2f, 2> LineGraph::createLineSegment(const sf::Vector2f& point)
+{
+	std::array<sf::Vector2f, 2> segment;
+
+	segment[0] = {point.x, point.y + lineThickness / 2};
+	segment[1] = {point.x, point.y - lineThickness / 2};
+
+	return segment;
 }
 
 void LineGraph::updateContent()
 {
+	mesh.clear();
+	
+	sf::FloatRect viewRect = view.getViewRect();
 
+	for (const auto& point : points)
+	{
+	//	if (!viewRect.contains(point))
+	//		continue;
+
+		sf::Vector2f pos = getPointPosition(point);
+
+		auto segment = createLineSegment(pos);
+		mesh.append({segment[0], lineColor});
+		mesh.append({segment[1], lineColor});
+	}
 }
 
 void LineGraph::render(sf::RenderTexture& canvas) const
 {
 	canvas.clear();
-	canvas.draw(mesh);
 	canvas.draw(grid);
+	canvas.draw(mesh);
 	canvas.display();
 }
